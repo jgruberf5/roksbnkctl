095cdb80b7ce1fd70d97e8f60172c19c042d00f2
jgruberf5 <jgruberf5@users.noreply.github.com>
2026-05-10 19:51:38 +0000

    deploy: 4039689c4fb5621ac4ec4f4930c24a0929b182f5

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-58e0a648.js',
+                path_to_root + 'searchindex-ea7a420d.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
