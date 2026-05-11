e3c3cf35b8b86b08fe6e8b9cd0ace96a01b36366
John Gruber <j.gruber@f5.com>
2026-05-11 14:57:02 +0000

    book: publish v1.0.1

--- book/searcher-c2a407aa.js
+++ book/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-7102f972.js',
+                path_to_root + 'searchindex-12b12dfe.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
