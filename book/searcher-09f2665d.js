f4874c23f9b441f5fef1ffd3961f3c7151b39585
John Gruber <john.t.gruber@gmail.com>
2026-08-05 14:07:50 -0500

    book: publish v1.36.0

--- book/searcher-09f2665d.js
+++ book/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-71ee849a.js',
+                path_to_root + 'searchindex-a133a368.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
